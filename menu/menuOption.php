6aa7806bf8110613a29dd36628f6402cd0096f14
AngelMatthewsJimenez <73416133+AngelMatthewsJimenez@users.noreply.github.com>
2020-12-05 23:58:37 -0400

    menuOption

---
+++ menu/menuOption.php
@@ -0,0 +1,69 @@
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <title> VOTACIONES </title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="..\Asset\css\bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="..\Asset\css\stylemenu.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+  <div class="container">
+
+    <div class="algo1 row">
+      <div class="col-sm-5">
+        <h4 class="vot2">Menu de</h4>
+        <h4 class="vot2">Opciones</h4>
+      </div>
+
+    </div>
+
+    <div class="row-md-6">
+      <a href="">
+        <button class="btn btn1 btn-lg btn-primary btn-warning" type="submit">Fecha Hora</button>
+      </a>
+    </div>
+    <div class="row-md-6">
+      <a href="">
+        <button class="btn btn1 btn-lg btn-primary btn-warning" type="submit">Estado del Sistema</button>
+      </a>
+    </div>
+    <div class="row-md-6">
+      <a href="">
+        <button class="btn btn1 btn-lg btn-primary btn-warning" type="submit">Cambio de Clave</button>
+      </a>
+    </div>
+
+    <div class="row">
+      <div class="col">
+      </div>
+      <div class="col-md-auto">
+      </div>
+      <div class="col col-lg-2">
+
+        <a href="#">
+          <button class="btn btn2 btn-primary btn-warning" type="submit">Atras</button>
+        </a>
+      </div>
+
+    </div>
+  </div>
+
+  </div>
+
+
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+  </script>
+  <script src="..\Asset\js\bootstrap.min.js"></script>
+
+
+</body>
+
+</html>
