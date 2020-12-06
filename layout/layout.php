b34267d4808f22c752a410512a96ed34ab78854a
AngelMatthewsJimenez <73416133+AngelMatthewsJimenez@users.noreply.github.com>
2020-12-05 20:27:13 -0400

    conexion BD

--- layout/layout.php
+++ layout/layout.php
@@ -76,7 +76,7 @@ public function PrintFooter() {
   ================================================== -->
   <!-- Placed at the end of the document so the pages load faster -->
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="Asset\js\bootstrap.min.js"></script>
+  <script src="{Asset\js\bootstrap.min.js"></script>
 
 
 </body>
