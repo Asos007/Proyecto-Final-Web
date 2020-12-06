b58c0edfbcba94f345d0b7b5d67074706fb92593
Javierlopez14 <39016714+Javierlopez14@users.noreply.github.com>
2020-12-05 22:35:34 -0400

    Holi
    Se agrego el menu principal o la pagina mas bien

---
+++ Sistem/menuprinci.php
@@ -0,0 +1,57 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <title> VOTACIONES </title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../Asset\css\bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="../Asset\css\stylemenu.css" rel="stylesheet" type="text/css">
+    
+
+  </head>
+  <body>
+    <div class="container">
+ 
+        <div class="algo row">
+        <div class="col-sm-8">
+            <h1 class="vot">Votaciones</h1>
+            <h2 class="vot">2020</h2>
+        </div>
+        <div class="col-sm-4">
+            <img class="photo" src="../Asset/img/Capture.PNG" alt="">
+        </div>
+        </div>
+
+            <div class="row-md-5">
+                <a href="../log/logpresecre.php">
+                    <button class="btn btn-lg btn-primary btn-warning" type="submit">INICIAR VOTACION</button>
+                </a>
+            </div>
+            <div class="row-md-5">
+                <button class="btn btn-lg btn-primary btn-warning" type="submit">SALIR DEL SISTEMA</button>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                </div>
+                <div class="col-md-auto">
+                </div>
+                <div class="col col-lg-2">
+                        <a href="#">
+                            <img class="photoo" src="../Asset/img/opciones.png" alt="">
+                        </a>
+                </div>
+            </div>
+    </div>
+
+    </div>
+
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="Asset\js\bootstrap.min.js"></script>
+
+
+</body>
+</html>
